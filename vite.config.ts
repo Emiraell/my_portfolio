c9a32edec2d63797b35503998827b4de7b39cdb8
emiraelDev <emmzex19@gmail.com>
2024-05-29 18:36:38 +0100

    add: deploy.uml

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my_portfolio",
   define: {
     // env variable from env file
     "process.env.VITE_MY_KEY": JSON.stringify(process.env.VITE_MY_KEY),
